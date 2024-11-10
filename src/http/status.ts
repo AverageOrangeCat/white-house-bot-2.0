478f3fe3ed114ff997ea7880c6443853fd5765ec
AverageOrangeCat <74761380+AverageOrangeCat@users.noreply.github.com>
2024-11-10 17:31:33 +0100

    Implement all gateway opcodes

--- src/http/status.ts
+++ src/http/status.ts
@@ -1,9 +1,8 @@
 /**
  * Source: https://discord.com/developers/docs/topics/opcodes-and-status-codes#http
- */ 
+ */
 
 export enum Status {
-
     /**
      * Description: The request completed successfully.
      */
